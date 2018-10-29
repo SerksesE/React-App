5e5c8f86254f2f9d14ad4d265ea1a9bb5679eb43
SerksesE <serkses@hotmail.com>
2018-10-29 16:21:48 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>Welcome to Codaisseur</h2>
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Codaisseur</h1>
+          <Title />
+
+
+
+
           {/* <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hey! Listen!</h2>
+      </div>
+    )
+  }
+}
